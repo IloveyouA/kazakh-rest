69cd76a8729574452b8bae962ecdabb505c2bfb6
Akay <kenesakai666@gmail.com>
2024-12-06 10:14:02 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/kazakh-rest" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/recipe/:id" element={<RecipePage />} />
